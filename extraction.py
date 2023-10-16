93292a49ed085e4bb5c7b4445fc990c99f0607be
Yeonsu Jung <victorr0618@naver.com>
2023-10-16 13:17:34 +0900

    Update extraction.py

--- extraction.py
+++ extraction.py
@@ -1,44 +1,22 @@
 # module
-import cv2
 from PIL import Image
 
 
 class img_map:
     def __init__(self, img):
-        self.grayscaleImg = cv2.imread(img, cv2.IMREAD_GRAYSCALE)
-        self.img = Image.open(img).convert("L")
+        self.__img = Image.open(img)
 
-    def show(self):
-        cv2.imshow("ROKMC_DOG", self.grayscaleImg)
-        cv2.waitKey()
-        cv2.destroyAllWindows()
-
-    def getDataList(self):
-        pixel_value = []
-        (height, width) = self.grayscaleImg.shape
-
-        for y in range(height):  # 모든 픽셀에 대한 grayScale을 가져옴
-            t = []
-            for x in range(width):
-                t.append(self.grayscaleImg[y, x])
-            pixel_value.append(t)
-
-        return pixel_value
-
-    def getData(self):
-        data = self.img.getdata()
-        return data
-
-    def printDataList(self):  # 잘 작동하는지 확인
-        t = self.getDataList()
-        for y in range(len(t)):
-            for x in range(len(t[0])):
-                print("%3d" % t[y][x], end=" ")
-            print()
-
+    def getPixel(self):
+        return self.img.getdata()
+    
+    @property
+    def img(self):
+        return self.__img
 
 # test code
 if __name__ == "__main__":
     a = img_map("img\\testImg.png")
-    a.printDataList()
-    a.show()
+    print(type(a.img))
+    print(type(Image.open("img\\testImg.png")))
+    # for item in a.getPixel():
+    #     print(item)
