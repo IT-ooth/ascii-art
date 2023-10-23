b9c927ecc834f8b05f2d3d77543c7a1f39568d70
sunshinetoyou <victorr0618@naver.com>
2023-10-23 23:21:45 +0900

    Update converter.py

--- ascii-art/src/converter.py
+++ ascii-art/src/converter.py
@@ -8,7 +8,7 @@ class Convert:
         else:
             return 0.00
         
-    def match(self, pixel) -> int:
+    def match(self, pixel: int) -> int:
         imgRatio = self.getImgRatio(pixel) # 한 pixel의 grayscale의 분포값
         dataSet = get_asciiDataset()
         
@@ -22,16 +22,33 @@ class Convert:
                 minNum = absNum  #지금 차이를 넣어준다.
                 # nearNum = data     #근사값에 지금 데이터를 넣어준다.
                 asciiNum = i
-        
+                if minNum == 0:     # 차이가 0 이라면 바로 ASCIINUM을 리턴
+                    return asciiNum
+
         return asciiNum
     
-    def getResult(self, pixels):
+    def getResult(self, pixels: list) -> list[chr]:
         result = []
-        for item in pixels:
-            self.match(item)
 
+        for item in pixels:
+            result.append(chr(self.match(item)))
+
+        return result
+
+# 테스트용
+def writeResult(result, imgSize):
+    with open("tests\\test_result.txt", 'w') as f:
+        i = 0
+
+        for h in range(imgSize[1]):
+            for w in range(imgSize[0]):
+                f.write(result[i])
+                i += 1
+            f.write("\n")
 
 if __name__ == "__main__":
     a = PreImage("tests\\resources\\ROKMC.jpg")
+    pixel_data = a.getPixel()
     b = Convert()
-    print(b.getImgRatio(240))
+
+    writeResult(b.getResult(pixel_data), a.size)
