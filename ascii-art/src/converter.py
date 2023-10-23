3483bddd0197ad2c7f395e02acb89bad202dd319
Yeonsu Jung <victorr0618@naver.com>
2023-10-23 16:33:24 +0900

    Update converter.py

--- ascii-art/src/converter.py
+++ ascii-art/src/converter.py
@@ -1,9 +1,37 @@
-import preImage
+from preImage import PreImage
 from asciidata import get_asciiDataset
 
-class ConvertingImage:
-    def __init__(self, pixels):
-        self.pixels = pixels
+class Convert:
+    def getImgRatio(self, pixel: int) -> float:
+        if pixel != 0:
+            return round(pixel / 255, 3)
+        else:
+            return 0.00
+        
+    def match(self, pixel) -> int:
+        imgRatio = self.getImgRatio(pixel) # 한 pixel의 grayscale의 분포값
+        dataSet = get_asciiDataset()
+        
+        minNum = 1
+        asciiNum = 0
+        
+        for i, data in dataSet.items():
+            absNum = abs(data-imgRatio) 
 
-    def match(self, pixel):
-        pass
+            if absNum < minNum:  #지금 차이가 전의 차이보다 작으면
+                minNum = absNum  #지금 차이를 넣어준다.
+                # nearNum = data     #근사값에 지금 데이터를 넣어준다.
+                asciiNum = i
+        
+        return asciiNum
+    
+    def getResult(self, pixels):
+        result = []
+        for item in pixels:
+            self.match(item)
+
+
+if __name__ == "__main__":
+    a = PreImage("tests\\resources\\ROKMC.jpg")
+    b = Convert()
+    print(b.getImgRatio(240))
