21e863eb67827429ac539aeecc9eaad557fe2f23
Yeonsu Jung <victorr0618@naver.com>
2023-10-23 14:43:27 +0900

    Update asciidata.py

---
+++ ascii-art/src/asciidata.py
@@ -0,0 +1,10 @@
+# ascii-dataset 를 가져오기
+import yaml
+
+def get_asciiDataset() -> dict:
+    with open("ascii-art\\resources\\ascii-dataset.yaml", 'r' , encoding='UTF8') as f:
+        file = yaml.full_load(f)
+        return file
+
+if __name__ == "__main__":
+    print(get_asciiDataset())
