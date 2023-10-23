80e48245984d032d0a2cf3924454ec7cca2c66f7
Yeonsu Jung <victorr0618@naver.com>
2023-10-23 16:33:21 +0900

    Update getDataset.py

--- tests/getDataset.py
+++ tests/getDataset.py
@@ -22,6 +22,6 @@ def getDataset() -> list:
 
 if __name__ == "__main__":
     a = getDataset()
-    print(a)
-    for i, k in enumerate(a):
-        print(str(i) + " : "+ str(k) + ",")
+    print(max(a))
+    # for i, k in enumerate(a):
+    #     print(str(i) + " : "+ str(k) + ",")
