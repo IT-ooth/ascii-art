ece98052aa358d25192c8603b5b739a7d40af66e
Yeonsu Jung <victorr0618@naver.com>
2023-10-24 15:36:32 +0900

    Update converter.py

--- ascii-art/src/converter.py
+++ ascii-art/src/converter.py
@@ -2,6 +2,7 @@ from preImage import PreImage
 from asciidata import get_asciiDataset
 
 class Convert:
+
     def getImgRatio(self, pixel: int) -> float:
         if pixel != 0:
             return round(pixel / 255, 3)
@@ -20,35 +21,22 @@ class Convert:
 
             if absNum < minNum:  #지금 차이가 전의 차이보다 작으면
                 minNum = absNum  #지금 차이를 넣어준다.
-                # nearNum = data     #근사값에 지금 데이터를 넣어준다.
                 asciiNum = i
+
                 if minNum == 0:     # 차이가 0 이라면 바로 ASCIINUM을 리턴
                     return asciiNum
 
         return asciiNum
     
-    def getResult(self, pixels: list) -> list[chr]:
+    def getResult(self, pixels: list) -> list:
         result = []
-
+        
         for item in pixels:
             result.append(chr(self.match(item)))
 
         return result
 
-# 테스트용
-def writeResult(result, imgSize):
-    with open("tests\\test_result.txt", 'w') as f:
-        i = 0
-
-        for h in range(imgSize[1]):
-            for w in range(imgSize[0]):
-                f.write(result[i])
-                i += 1
-            f.write("\n")
-
 if __name__ == "__main__":
     a = PreImage("tests\\resources\\ROKMC.jpg")
     pixel_data = a.getPixel()
     b = Convert()
-
-    writeResult(b.getResult(pixel_data), a.size)
