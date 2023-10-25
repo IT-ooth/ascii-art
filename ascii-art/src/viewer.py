4a8bddaa4e149258faa701d08c1e6dbaded29c89
Jun Hyuk <rugsn12345@gmail.com>
2023-10-25 23:53:17 +0900

    viewer.py GUI 구성

--- ascii-art/src/viewer.py
+++ ascii-art/src/viewer.py
@@ -1,10 +1,8 @@
 from functools import singledispatch
 from abc import ABCMeta, abstractmethod
-from PyQt5 import QtWidgets, uic
+from tkinter import ttk
+import tkinter
 import converter
-import yaml
-import sys
-
 # UI 추상클래스
 class UI(metaclass = ABCMeta):
     url = "ascii-art/resources"
@@ -18,64 +16,65 @@ class TUI(UI):
     def getUrl(self) -> str:
         return super().getUrl()
 
+class GUI(UI):
 
+    def getUrl(self) -> str:
+        return super().getUrl()
+    
 class Viewer():
 
     convertor = converter.Convert()
+    tk = tkinter.Tk()
 
     def __init__(self) -> None:
         
         self.show = singledispatch(self.show)
         self.show.register(TUI, self.__showTUI)
-    
+        self.show.register(GUI, self.__showGUI)
+
+        self.setUI()
+
+        self.tk.mainloop()
+
     # show를 메서드 오버로딩해서 구성
     
-    def show(self, arg: UI):pass
+    # GUI 구성
+    def setUI(self):
+        self.selectUIcombo = ttk.Combobox(self.tk, height = 2, values = ["GUI", "TUI"], state = "readonly")
+        self.selectUIcombo.current(0)
+        self.selectUIcombo.grid(row = 0, column = 0)
+
+        self.urlEntry = tkinter.Entry(self.tk, width = 80)
+        self.urlEntry.grid(row = 0, column = 1)
+        
+        self.calBtn = tkinter.Button(self.tk, text = "변환", command = lambda: self.show(UI))
+        self.calBtn.grid(row = 0, column = 2)
+        
+        self.textbox = tkinter.Text(self.tk, font = ("Times", 9))
+        self.textbox.grid(row = 1, column = 0, columnspan = 10)
+
+    def show(self, arg: UI):
+
+        if self.selectUIcombo.get() == "GUI":
+            self.show(GUI())
+        
+        elif self.selectUIcombo.get() == "TUI":
+
+            self.show(TUI())
 
     def __showTUI(self, arg: TUI):
-        
+
         with open(arg.getUrl() + "result.txt", "w") as file:
-            for line in self.convertor.getPixels(arg.getUrl()):
+            for line in self.convertor.getPixels(self.urlEntry.get()):
                 file.write("".join(line) + "\n")
 
+    def __showGUI(self, arg: GUI):
 
-window_form = uic.loadUiType("ascii-art/resources/window_UI.ui")[0]
+        for line in self.convertor.getPixels(self.urlEntry.get()):
+            self.textbox.insert("end", "".join(line))
 
 
-# 미완성
-class MainWindow(QtWidgets.QMainWindow, window_form):
-
-    def __init__(self):
-        super().__init__()
         
-        self.setupUi()
-        
-        self.initUI()
-    
-    def initUI(self):
-        
-        self.setUIoption()
 
-        self.setWindowTitle(self.data["title"])
-        self.resize(self.data["size"][0], self.data["size"][1])
-        self.center()
-        self.show()
-        
-    
-    def setUIoption(self):
+a = Viewer()
 
-        with open("ascii-art/resources/window_config.yml", encoding = 'utf-8') as file:
-            
-            self.__data = yaml.load(file, Loader = yaml.FullLoader)
-    
-
-    def center(self):
-
-        qr = self.frameGeometry()
-        qr.moveCenter(QtWidgets.QDesktopWidget().availableGeometry().center())
-
-        self.move(qr.topLeft())
-
-    @property
-    def data(self): 
-        return self.__data
