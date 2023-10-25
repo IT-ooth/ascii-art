f1e65e3471198ff47f1b9b6c44de849b63488b3f
qofo <junseolee02@gmail.com>
2023-10-25 22:34:34 +0900

    update converter.py

--- ascii-art/src/converter.py
+++ ascii-art/src/converter.py
@@ -41,38 +41,19 @@ class Convert:
 
     
     def getPixels(self, img_path: str) -> list:
+
         converting_image = PreImage(img_path, 0.7)
         image_pixels = converting_image.getPixel()
+
         one_dim = self.matchbyList(image_pixels)
         two_dim = []
 
         x, y = converting_image.img.size
-
+        
         for i in range(y):
             two_dim.append(one_dim[i*x:(i+1)*x])
 
         return two_dim
-    
-            
-# 테스트용 함수들
-def writeResult(result):
-    with open("result.txt", 'w', encoding='UTF-8') as f:
-    
-        '''for line in result:
-            f.write(str(line) + "\n")'''
-            
-        for i in result:
-            for j in i:
-                # 정석적인 출력 방식. 대신 텍스트 파일로 보면 줄이 밀려있음
-                f.write(str(j)+' ')
-
-                # 결과를 예쁘게 보고 싶을 때
-                #f.write(j+j+j+'\t')
-            f.write("\n")
-            
-
 
 if __name__ == "__main__":
     a = Convert()
-    writeResult(a.getPixels("tests/resources/ROKMC.jpg"))
-    #writeResult(a.getPixels("../../tests/resources/ROKMC.jpg"))
