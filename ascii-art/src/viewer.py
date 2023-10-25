1c2740a2ba3fc3053f425375c9ac87848d2cc35b
Jun Hyuk <rugsn12345@gmail.com>
2023-10-25 22:45:29 +0900

    converter.py 결합

--- ascii-art/src/viewer.py
+++ ascii-art/src/viewer.py
@@ -1,5 +1,9 @@
 from functools import singledispatch
 from abc import ABCMeta, abstractmethod
+from PyQt5 import QtWidgets, uic
+import converter
+import yaml
+import sys
 
 # UI 추상클래스
 class UI(metaclass = ABCMeta):
@@ -14,7 +18,10 @@ class TUI(UI):
     def getUrl(self) -> str:
         return super().getUrl()
 
+
 class Viewer():
+
+    convertor = converter.Convert()
 
     def __init__(self) -> None:
         
@@ -26,7 +33,49 @@ class Viewer():
     def show(self, arg: UI):pass
 
     def __showTUI(self, arg: TUI):
-
+        
         with open(arg.getUrl() + "result.txt", "w") as file:
-            for line in Convertor.getPixels():
+            for line in self.convertor.getPixels(arg.getUrl()):
                 file.write("".join(line) + "\n")
+
+
+window_form = uic.loadUiType("ascii-art/resources/window_UI.ui")[0]
+
+
+# 미완성
+class MainWindow(QtWidgets.QMainWindow, window_form):
+
+    def __init__(self):
+        super().__init__()
+        
+        self.setupUi()
+        
+        self.initUI()
+    
+    def initUI(self):
+        
+        self.setUIoption()
+
+        self.setWindowTitle(self.data["title"])
+        self.resize(self.data["size"][0], self.data["size"][1])
+        self.center()
+        self.show()
+        
+    
+    def setUIoption(self):
+
+        with open("ascii-art/resources/window_config.yml", encoding = 'utf-8') as file:
+            
+            self.__data = yaml.load(file, Loader = yaml.FullLoader)
+    
+
+    def center(self):
+
+        qr = self.frameGeometry()
+        qr.moveCenter(QtWidgets.QDesktopWidget().availableGeometry().center())
+
+        self.move(qr.topLeft())
+
+    @property
+    def data(self): 
+        return self.__data
