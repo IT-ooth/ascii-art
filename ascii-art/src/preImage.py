d83a155c05a6f7582c2ffa9bf33284527910781e
Yeonsu Jung <victorr0618@naver.com>
2023-10-25 18:01:42 +0900

    Update preImage.py

--- ascii-art/src/preImage.py
+++ ascii-art/src/preImage.py
@@ -3,33 +3,32 @@ from PIL import Image
 
 
 class PreImage:
-    def __init__(self, path: str):
-        self.__img = self.setImage(path)
+    def __init__(self, path: str, size_ratio):
+        self.__img = self.setImage(path, size_ratio)
 
     @property
     def img(self):  # img 불러오기
         return self.__img
 
-    @property
-    def size(self) -> tuple:  # 주어진 이미지의 사이즈
-        return self.img.size
+    # img setting
+    def setImage(self, path: str, size_ratio=1):
+        img = Image.open(path)
 
-    @property
-    def area(self) -> int:  # 주어진 이미지의 넓이
-        w, h = self.img.size
-        return w * h
-
-    def setImage(self, path):  # 경로에 있는 이미지를 객체로 반환
-        return Image.open(path)
-
+        # img resize
+        def resize(img, size_ratio):
+            x, y = img.size
+            return img.resize((int(x*size_ratio), int(y*size_ratio)))
+    
+        return resize(img, size_ratio)
+    
     def getGrayscale(self):  # 이미지를 grayScale로 만듦
         return self.img.convert("L")
 
-    def getPixel(self):  # 이미지의 grayScale 데이터 받아오기
+    def getPixel(self):  # 이미지의 grayScale 데이터 -> iterator
         return self.getGrayscale().getdata()
 
 
 # test code
 if __name__ == "__main__":
-    a = PreImage("img\\ROKMC_dog.jpg")
-    print(a.getPixel())
+    a = PreImage("tests\\resources\\ROKMC.jpg", 0.5)
+    a.img.show()
