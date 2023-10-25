dc6db00d8895e0fce44680e56cafc9edc85c8ecc
qofo <junseolee02@gmail.com>
2023-10-26 01:27:29 +0900

    converter.py 함수명 변경

--- ascii-art/src/converter.py
+++ ascii-art/src/converter.py
@@ -5,7 +5,7 @@ class Convert:
     
     dataset = get_asciiDataset()
 
-    #ascii-dataset.yml의 값이 0~1이라서 이거를 0~255 스케일로 변경
+    #ascii-dataset.yml의 값이 0~255이라서 이거를 0~1 스케일로 변경
     def getImgRatio(self, pixel: int) -> float:
         if pixel != 0:
             return round(pixel / 255, 3)
@@ -14,11 +14,12 @@ class Convert:
         
     #픽셀 하나의 grayscale 받아서 그것에 맞는 아스키 문자 반환
     def matchByOne(self, pixel: int) -> int:
-        imgRatio = self.getImgRatio(pixel) # 한 pixel의 grayscale의 분포값
-        #imgRatio = pixel
+        #imgRatio = self.getImgRatio(pixel) # 한 pixel의 grayscale의 분포값
+        imgRatio = pixel
 
-        minNum = 1
+        minNum = 255
         asciiNum = 0
+        
         
         for key, data in self.dataset.items():
             absNum = abs(data-imgRatio) 
@@ -59,15 +60,15 @@ class Convert:
 if __name__ == "__main__":
     # 테스트용 함수들
     def writeResult(result):
-        with open("tests/test_result.txt", 'w', encoding='UTF-8') as f:
+        with open("result.txt", 'w', encoding='ASCII') as f:
             for i in result:
                 for j in i:
                     # 정석적인 출력 방식. 대신 텍스트 파일로 보면 줄이 밀려있음
-                    f.write(j+' ')
+                    f.write(str(j)+' ')
 
                     # 결과를 예쁘게 보고 싶을 때
                     #f.write(j+j+j+'\t')
                 f.write("\n")
     a = Convert()
-    writeResult(a.getAsciis("tests/resources/ROKMC.jpg", 0.7))
+    writeResult(a.getAsciis("tests/resources/pepe.jpg",0.2))
     #writeResult(a.getPixels("../../tests/resources/ROKMC.jpg"))
