7bb31934d1dcc533ff2e2126f3409d740fb9261b
Jun Hyuk <rugsn12345@gmail.com>
2023-10-27 10:00:49 +0900

    viewer.py 분할 및 converter.py import 구문 변경

--- ascii-art/main.py
+++ ascii-art/main.py
@@ -1,9 +1,35 @@
-# 소스코드
-# 구조
-# import ~~~
-#
-# def main():
-#   ~~~
-#
-# if __name__ == "__main__"
-#   main()
+from src.converter import Convert
+import src.viewer
+import tkinter
+
+
+
+class Main(tkinter.Tk):
+
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.setUI()
+        self.mainloop()
+
+        return None
+    
+    def setUI(self) -> None:
+
+        urlEntry = tkinter.Entry(self, width = 80)
+        urlEntry.grid(row = 0, column = 0)
+        
+        calBtn = tkinter.Button(self, text = "변환", command = lambda:self.show(urlEntry.get()))
+        calBtn.grid(row = 0, column = 1)
+    
+        return None
+
+    def show(self, url:str) -> None:
+        
+        convertor = Convert()
+        try: src.viewer.Viewer(convertor.getAsciis(url))
+        except FileNotFoundError: pass
+        
+
+
+a = Main()
--- ascii-art/src/viewer.py
+++ ascii-art/src/viewer.py
@@ -1,81 +1,36 @@
-from functools import singledispatch
-from abc import ABCMeta, abstractmethod
-from tkinter import ttk
+import tkinter.font
 import tkinter
-import converter
 
-# UI 추상클래스
-class UI(metaclass = ABCMeta):
-    url = "ascii-art/resources"
+class Viewer(tkinter.Toplevel):
 
-    @abstractmethod
-    def getUrl(self) -> str:
-        return self.url
+    def __init__(self, asciis: list) -> None:
+        super().__init__()
+
+        self.setUI(asciis)
+
+        self.focus()
+        self.grab_set()
+
+        return None
     
-class TUI(UI):
+    def setUI(self, asciis: list) -> None:
+        menubar = tkinter.Menu(self)
+        self.config(menu = menubar)
 
-    def getUrl(self) -> str:
-        return super().getUrl()
+        font = tkinter.font.Font(family="MS Gothic", size = 2)
+        label = tkinter.Label(self, text = self.listTotext(asciis), font = font)
+        label.pack()
+        fileMenu = tkinter.Menu(menubar)
+        fileMenu.add_command(label = "Save", command = self.save)
+        menubar.add_cascade(label = "File", menu = fileMenu)
 
-class GUI(UI):
+    def save(self):
+        pass
 
-    def getUrl(self) -> str:
-        return super().getUrl()
-    
-class Viewer():
+    def listTotext(self, asciis: list):
+        string = str()
 
-    convertor = converter.Convert()
-    tk = tkinter.Tk()
-
-    def __init__(self) -> None:
+        for line in asciis:
+            string += "".join(line) + "\n"
         
-        self.show = singledispatch(self.show)
-        self.show.register(TUI, self.__showTUI)
-        self.show.register(GUI, self.__showGUI)
-
-        self.setUI()
-
-        self.tk.mainloop()
-
-    # show를 메서드 오버로딩해서 구성
-    
-    # GUI 구성
-    def setUI(self):
-        self.selectUIcombo = ttk.Combobox(self.tk, height = 2, values = ["GUI", "TUI"], state = "readonly")
-        self.selectUIcombo.current(0)
-        self.selectUIcombo.grid(row = 0, column = 0)
-
-        self.urlEntry = tkinter.Entry(self.tk, width = 80)
-        self.urlEntry.grid(row = 0, column = 1)
-        
-        self.calBtn = tkinter.Button(self.tk, text = "변환", command = lambda: self.show(UI))
-        self.calBtn.grid(row = 0, column = 2)
-        
-        self.textbox = tkinter.Text(self.tk, font = ("Times", 9))
-        self.textbox.grid(row = 1, column = 0, columnspan = 10)
-
-    def show(self, arg: UI):
-
-        if self.selectUIcombo.get() == "GUI":
-            self.show(GUI())
-        
-        elif self.selectUIcombo.get() == "TUI":
-
-            self.show(TUI())
-
-    def __showTUI(self, arg: TUI):
-
-        with open(arg.getUrl() + "result.txt", "w") as file:
-            for line in self.convertor.getPixels(self.urlEntry.get()):
-                file.write("".join(line) + "\n")
-
-    def __showGUI(self, arg: GUI):
-
-        for line in self.convertor.getPixels(self.urlEntry.get()):
-            self.textbox.insert("end", "".join(line))
-
-
-        
-
-a = Viewer()
-
+        return string
